6c39358c6bb507d321ddad283736535ac0fdde2a
zhouzhengrui <zhouzhengrui@gmail.com>
2016-12-21 00:29:51 +0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,35 @@
-jQuery(document).ready(function ($) {
+
+jQuery(document).ready(function($) {
 
     // menu
 
     // $("ul.menu").superfish({
     //     hoverClass: "sfHover",
     //     delay: 800,
-    //     animation: {opacity: "show"},
-    //     animationOut: {opacity: "hide"},
+    //     animation: {
+    //         opacity: "show"
+    //     },
+    //     animationOut: {
+    //         opacity: "hide"
+    //     },
     //     speed: "normal",
     //     speedOut: "fast",
     //     cssArrows: true
     // });
+
+    $("ul.topLink").superfish({
+        hoverClass: "sfHover",
+        delay: 800,
+        animation: {
+            opacity: "show"
+        },
+        animationOut: {
+            opacity: "hide"
+        },
+        speed: 0,
+        speedOut: "fast",
+        cssArrows: true
+    });
 
     // slider
 
@@ -28,58 +47,105 @@ jQuery(document).ready(function ($) {
         nextText: "&#xe8ec;",
         maxwidth: ""
     });
-    $(".slider a").attr("href","javascript:;");
+    $(".slider a").attr("href", "javascript:;");
     $(".slider_nav.prev").addClass("fontIcon icon-angle-left");
     $(".slider_nav.next").addClass("fontIcon icon-angle-right");
     var sliderImageLoad = [];
-    $(".sliderBox img").each(function () {
+    $(".sliderBox img").each(function() {
         var sliderImage = $.Deferred();
         $(this).load(sliderImage.resolve);
         sliderImageLoad.push(sliderImage);
     });
-    $.when.apply(null, sliderImageLoad).done(function () {
+    $.when.apply(null, sliderImageLoad).done(function() {
         $(".fontIconLoading").remove();
     });
 
-    // layout
-
-
     // cover
 
-    $(".cover01").each(function () {
-        $(this).hover(function () {
-            $(".coverBackground", this).stop().animate({opacity: '.75'}, {queue: false, duration: 200});
-            $(".coverTitle", this).stop().animate({bottom: '55%'}, {queue: false, duration: 300});
-            $(".coverButton", this).stop().animate({top: '55%'}, {queue: false, duration: 300});
-        }, function () {
-            $(".coverBackground", this).stop().animate({opacity: '0'}, {queue: false, duration: 200});
-            $(".coverTitle", this).stop().animate({bottom: '100%'}, {queue: false, duration: 300});
-            $(".coverButton", this).stop().animate({top: '100%'}, {queue: false, duration: 300});
+    $(".cover01").each(function() {
+        $(this).hover(function() {
+            $(".coverBackground", this).stop().animate({
+                opacity: '.75'
+            }, {
+                queue: false,
+                duration: 200
+            });
+            $(".coverTitle", this).stop().animate({
+                bottom: '55%'
+            }, {
+                queue: false,
+                duration: 300
+            });
+            $(".coverButton", this).stop().animate({
+                top: '55%'
+            }, {
+                queue: false,
+                duration: 300
+            });
+        }, function() {
+            $(".coverBackground", this).stop().animate({
+                opacity: '0'
+            }, {
+                queue: false,
+                duration: 200
+            });
+            $(".coverTitle", this).stop().animate({
+                bottom: '100%'
+            }, {
+                queue: false,
+                duration: 300
+            });
+            $(".coverButton", this).stop().animate({
+                top: '100%'
+            }, {
+                queue: false,
+                duration: 300
+            });
         });
     });
 
-    $(".cover02").each(function () {
+    $(".cover02").each(function() {
         var cover02Offset01 = $(this).height();
         var cover02Offset02 = $(".coverBox", this).innerHeight();
         $(this).css("height", cover02Offset01);
-        $(this).hover(function () {
-            $(".imgBox img", this).stop().animate({marginTop: -cover02Offset02 / 2}, {queue: false, duration: 300});
-            $(".coverBox", this).stop().animate({top: cover02Offset01 - cover02Offset02}, {queue: false, duration: 300});
-        }, function () {
-            $(".imgBox img", this).stop().animate({marginTop: '0'}, {queue: false, duration: 300});
-            $(".coverBox", this).stop().animate({top: '100%'}, {queue: false, duration: 300});
+        $(this).hover(function() {
+            $(".imgBox img", this).stop().animate({
+                marginTop: -cover02Offset02 / 2
+            }, {
+                queue: false,
+                duration: 300
+            });
+            $(".coverBox", this).stop().animate({
+                top: cover02Offset01 - cover02Offset02
+            }, {
+                queue: false,
+                duration: 300
+            });
+        }, function() {
+            $(".imgBox img", this).stop().animate({
+                marginTop: '0'
+            }, {
+                queue: false,
+                duration: 300
+            });
+            $(".coverBox", this).stop().animate({
+                top: '100%'
+            }, {
+                queue: false,
+                duration: 300
+            });
         });
     });
 
     // masonry
 
-    var $container = $('.masonryBox');
-    $container.imagesLoaded( function(){
-        $container.masonry({
-            itemSelector: '.masonryGroup',
-            fitWidth: true
-        });
-    });
+    // var $container = $('.masonryBox');
+    // $container.imagesLoaded( function(){
+    //     $container.masonry({
+    //         itemSelector: '.masonryGroup',
+    //         fitWidth: true
+    //     });
+    // });
 
     // fancy box
 
@@ -95,7 +161,7 @@ jQuery(document).ready(function ($) {
 
     // parallax
 
-    $(".parallaxText").each(function () {
+    $(".parallaxText").each(function() {
         var parallaxTextOffset = $(".parallaxText").height();
         $(this).css("margin-top", -parallaxTextOffset / 2);
     });
@@ -116,9 +182,11 @@ jQuery(document).ready(function ($) {
 
     // anchor
 
-    $(".anchor").click(function () {
+    $(".anchor").click(function() {
         var id = '#' + $(this).attr("rel");
-        $("html, body").animate({scrollTop: $(id).offset().top}, 600);
+        $("html, body").animate({
+            scrollTop: $(id).offset().top
+        }, 600);
     });
 
     // form
